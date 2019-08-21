d2f236fa59161efe1b239a1f210bc7c921a956e8
johnteruya <jteruya9@gmail.com>
2019-08-20 21:05:15 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ cursomc/src/main/java/com/nelioalves/cursomc/domain/Enums/TipoCliente.java
@@ -0,0 +1,37 @@
+package com.nelioalves.cursomc.domain.Enums;
+
+public enum TipoCliente {
+	
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+	
+	private Integer cod;
+	private String descricao;
+	
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	
+	public int getCod() {
+		return cod;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static TipoCliente toEnum (Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for(TipoCliente x : TipoCliente.values()) {
+			if(cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Código Inválido" + cod);
+	}
+}
