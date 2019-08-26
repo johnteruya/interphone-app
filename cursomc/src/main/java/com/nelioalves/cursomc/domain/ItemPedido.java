5412a6a080a60959188ea2f10eeb1b261038dac3
johnteruya <jteruya9@gmail.com>
2019-08-26 19:43:56 -0300

    ItemPedido e ItemPedidoPK

---
+++ cursomc/src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -0,0 +1,105 @@
+package com.nelioalves.cursomc.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Entity
+public class ItemPedido implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	/* Id embutido em outro Id Auxiliar */
+	@EmbeddedId
+	private ItemPedidoPK id = new ItemPedidoPK();
+	
+	private Double Desconto;
+	private Integer Quantidade;
+	private Double Preco;
+	
+	public ItemPedido() {
+		
+	}
+
+	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
+		super();
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		Desconto = desconto;
+		Quantidade = quantidade;
+		Preco = preco;
+	}
+
+	
+	/*
+	 * getters and setters individuais de pedido e produto separados
+	 */	
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+	
+	public Produto getProduto(){
+		return id.getProduto();
+	}
+	
+	
+	
+	public ItemPedidoPK getId() {
+		return id;
+	}
+
+	public void setId(ItemPedidoPK id) {
+		this.id = id;
+	}
+
+	public Double getDesconto() {
+		return Desconto;
+	}
+
+	public void setDesconto(Double desconto) {
+		Desconto = desconto;
+	}
+
+	public Integer getQuantidade() {
+		return Quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		Quantidade = quantidade;
+	}
+
+	public Double getPreco() {
+		return Preco;
+	}
+
+	public void setPreco(Double preco) {
+		Preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPedido other = (ItemPedido) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
