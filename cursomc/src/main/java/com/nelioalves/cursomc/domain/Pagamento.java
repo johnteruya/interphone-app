2bac11ccf9cd5317272d4f54cdd037f830cd0ac9
johnteruya <jteruya9@gmail.com>
2019-09-08 21:49:37 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- cursomc/src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
+++ cursomc/src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
@@ -35,7 +35,7 @@ public class Pagamento  implements Serializable{
 	public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
 		super();
 		this.id = id;
-		this.estado = estado.getCod();
+		this.estado = (estado==null) ? null : estado.getCod();
 		this.pedido = pedido;
 	}
 
